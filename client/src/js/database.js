dad3259ad6ab1395fc1060eb5b061d8252d43ae3
William Kerby <w.kerby99@gmail.com>
2023-05-17 04:10:44 -0400

    start put method for indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,17 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1)
+
+  }
+
+  catch (err) {
+    console.error('putDb not implemented')
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
